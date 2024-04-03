972a591adde411af36aa5d8e64780c68f784101c
HH-065 <165210208+HH-065@users.noreply.github.com>
2024-04-03 23:40:16 +0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 10000; // Dollar
+let myPin = 1506;
+
+let pinAnswer = await inquirer.prompt(
+    [
+        {
+            name: "pin",  
+            message: "enter your pin",   
+            type: "number",
+        }
+    ]
+);
+
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!!!");
+
+    let operationAns = await inquirer.prompt(
+        [
+            {
+                name:"operation",
+                message:"please select option",
+                type:"list",
+                choices: ["withdraw", "check balance"]
+            }
+        ]
+    );
+
+    console.log(operationAns);
+
+    if (operationAns.operation === "withdraw") {
+        let operationAns = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message: "enter your amount",
+                    type: "number",
+                }
+            ]
+        );
+
+       myBalance -= operationAns.amount;
+
+       console.log("Your remaining balance is: "+ myBalance)
+
+    } else if (operationAns.operation === "check balance"){
+        console.log("yourbalance is: " +  myBalance)
+    };
+}
+
+else {
+    console.log("Incorrect pin number");
+}
